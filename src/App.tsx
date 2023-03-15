af068883d943178e97e4725ed363e034431234f8
Your Namecl <you@example.com>
2023-03-15 10:32:35 +0100

    created hamburgerMenu component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { Nav } from "./components/nav/Nav";
 import { BackgroundWrapper } from "./components/styled/Wrappers";
 import { Footer } from "./components/footer/Footer";
+import { HamburgerMenu } from "./components/hamburger/Hamburger";
 
 function App() {
   return (
     <>
       <BackgroundWrapper>
         <header>
+          <HamburgerMenu></HamburgerMenu>
           <Nav></Nav>
         </header>
         <main>
---
+++ src/components/hamburger/Hamburger.tsx
@@ -0,0 +1,8 @@
+import { Turn as Hamburger } from "hamburger-react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { FONT_COLOR } from "../styled/Colors";
+
+export const HamburgerMenu = () => {
+  return <></>;
+};
