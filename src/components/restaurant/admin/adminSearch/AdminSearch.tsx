cd934eaddd2ccff5a714e0ddb54cb1c94b976461
Your Namecl <you@example.com>
2023-03-17 14:06:31 +0100

    changed button name

--- src/components/restaurant/admin/adminSearch/AdminSearch.tsx
+++ src/components/restaurant/admin/adminSearch/AdminSearch.tsx
@@ -8,7 +8,7 @@ import {
 import { Calendar } from "react-calendar";
 import { AdminContext } from "../Admin";
 import { Link } from "react-router-dom";
-import { ButtonFade } from "../../../styled/Buttons";
+import { Button } from "../../../styled/Buttons";
 
 export const AdminSearch = () => {
   const { handleDate, getAdminBookings } = useContext(AdminContext);
@@ -22,9 +22,9 @@ export const AdminSearch = () => {
       <SearchTableButtonWrapper>
         <Link to="/admin/bookings">
           {" "}
-          <ButtonFade onClick={getAdminBookings} height="50px" width="200px">
+          <Button onClick={getAdminBookings} height="50px" width="200px">
             Sök bokningar
-          </ButtonFade>
+          </Button>
         </Link>
       </SearchTableButtonWrapper>
     </BookingWrapper>
