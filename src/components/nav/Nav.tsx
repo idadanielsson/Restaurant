822e535da5fd52516dd7a8204332748144434073
Jesper <jesperhardin@gmail.com>
2023-03-17 13:23:42 +0100

    added hamburgerclose

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -39,16 +39,40 @@ export const Nav = () => {
       <div className={isOpen ? "navActive" : "nav"}>
         <NavList justify="end" marginRight="30px">
           <NavItem direction="column" gap="20px" fontSize="25px">
-            <NavLink color={FONT_COLOR} to="/">
+            <NavLink
+              onClick={() => {
+                setOpen(false);
+              }}
+              color={FONT_COLOR}
+              to="/"
+            >
               Hem
             </NavLink>
-            <NavLink color={FONT_COLOR} to="/menu/1">
+            <NavLink
+              onClick={() => {
+                setOpen(false);
+              }}
+              color={FONT_COLOR}
+              to="/menu/1"
+            >
               Meny
             </NavLink>
-            <NavLink color={FONT_COLOR} to="/booking">
+            <NavLink
+              onClick={() => {
+                setOpen(false);
+              }}
+              color={FONT_COLOR}
+              to="/booking"
+            >
               Bokning
             </NavLink>
-            <NavLink color={FONT_COLOR} to="/contact">
+            <NavLink
+              onClick={() => {
+                setOpen(false);
+              }}
+              color={FONT_COLOR}
+              to="/contact"
+            >
               Kontakt
             </NavLink>
           </NavItem>
